8df625b31506af6d328dbe3c5e0e3bfadf8cb251
yongji.yang <yongji.yang@kcl.ac.uk>
2022-10-09 22:21:56 +0100

    Modified constraints

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-	name = models.CharField(max_length = 30, unique = True)
+	name = models.CharField(max_length = 30)
 	description = models.CharField(max_length = 120, blank = True)
 	quantity = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)])
